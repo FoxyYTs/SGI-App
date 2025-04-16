1a2e15f4faba0de3bb877a57503de0acf725aabe
FoxyYTs <foxy200442@gmail.com>
2025-04-15 21:18:21 -0500

    `Added .env to .gitignore, deleted Ordenador/conexion.py and updated Ordenador/test.py to import mysql.connector and use the deleted file's function`

--- Ordenador/test.py
+++ Ordenador/test.py
@@ -1,8 +1,27 @@
-import os
-import subprocess
+import mysql.connector
+import conexion
 
-# Ruta a designer.exe
-designer_path = os.path.join(os.path.dirname(os.__file__), "site-packages", "PySide6", "designer.exe")
+def register(entry_correo, entry_usuario, entry_clave, entry_con_clave):
+    nombre = "Ejemplo"
+    ubicacion = 1
+    unidad_medida = 1
 
-# Ejecutar Qt Designer
-subprocess.Popen([designer_path])
+    try:
+        mydb = conexion.conectar()
+        mycursor = mydb.cursor()
+
+        sql = """INSERT INTO implemento 
+                (nombre_implemento, ubicacion_fk, und_medida_fk) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        
+        val = (nombre, ubicacion, unidad_medida)
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+
+    except mysql.connector.Error as err:
+        print("Error de conexión:", err)
+    finally:
+        if mydb.is_connected():
+            mycursor.close()
+            mydb.close()
